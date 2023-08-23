561b2fc6a913ae8e5ff2f290639d56c009c4083c
iamatos3 <matosian93@gmail.com>
2023-08-23 13:17:37 -0400

    Updated data to pull all personal projects for future fetching
    - included banner images in the assets folder as well

--- src/data.js
+++ src/data.js
@@ -1,22 +1,87 @@
 const projects = [
   {
-    title: 'birthday buddy',
-    url: 'https://react-vite-projects-1-birthday-buddy.netlify.app/',
-    image: './assets/birthday.png',
+    title: 'react todo app',
+    url: 'https://ians-react-task.netlify.app/',
+    image: './assets/react-todo-app.png',
   },
   {
-    title: 'tours',
-    url: 'https://react-vite-projects-2-tours.netlify.app/',
-    image: './assets/tours.png',
+    title: 'backroads',
+    url: 'https://backroads-ce.netlify.app/',
+    image: './assets/Backroads-banner.png',
   },
   {
-    title: 'reviews',
-    url: 'https://react-vite-projects-3-reviews.netlify.app/',
-    image: './assets/reviews.png',
+    title: 'our tours',
+    url: 'https://our-tours-ot.netlify.app/',
+    image: './assets/our-tours-banner.png',
   },
   {
-    title: 'questions',
-    url: 'https://react-vite-projects-4-accordion.netlify.app/',
-    image: './assets/questions.png',
+    title: 'birthday bud',
+    url: 'https://birthdaybud.netlify.app/',
+    image: './assets/birthday-banner.png',
+  },
+  {
+    title: 'colleague reviews',
+    url: 'https://colleague-reviews.netlify.app/',
+    image: './assets/colleague-banner.png',
+  },
+  {
+    title: 'faq questions',
+    url: 'https://faq-questions.netlify.app/',
+    image: './assets/FAQ-banner.png',
+  },
+  {
+    title: 'captains place',
+    url: 'https://captainsplace.netlify.app/',
+    image: './assets/captains-banner.png',
+  },
+  {
+    title: 'job tabbing',
+    url: 'https://job-tabbing.netlify.app/',
+    image: './assets/job-tabs-banner.png',
+  },
+  {
+    title: 'slider carousel',
+    url: 'https://slider-carousel-sl.netlify.app/',
+    image: './assets/slider-carousel-sl-banner.png',
+  },
+  {
+    title: 'dummy ipsum',
+    url: 'https://dummy-ipsum.netlify.app/',
+    image: './assets/dummyip-banner.png',
+  },
+  {
+    title: 'color generator',
+    url: 'https://color-generator-cg.netlify.app/',
+    image: './assets/cg-banner.png',
+  },
+  {
+    title: 'grocery pal',
+    url: 'https://grocerypal-gp.netlify.app/',
+    image: './assets/gp-banner.png',
+  },
+  {
+    title: 'navbar',
+    url: 'https://navbar-nb.netlify.app/',
+    image: './assets/navbar-banner.png',
+  },
+  {
+    title: 'sidebar modal',
+    url: 'https://sidebarmodal-sm.netlify.app/',
+    image: './assets/sidebarbanner.png',
+  },
+  {
+    title: 'strapi',
+    url: 'https://strapi-im.netlify.app/',
+    image: './assets/strapi-banner.png',
+  },
+  {
+    title: 'à la carte',
+    url: 'https://alacarte-im.netlify.app/',
+    image: './assets/a-la-carte-banner.png',
+  },
+  {
+    title: 'unsplashed images',
+    url: 'https://unsplashed-images.netlify.app/',
+    image: './assets/ui-banner.png',
   },
 ];
---
+++ src/fetchProjects.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: 'rf0op44d2k2s',
+  environment: 'master',
+  accessToken: 'JJHTsK70yuHGbtSOWZHZGN8urcs80B0Lh6041F40KR4',
+});
+
+const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true)
+  const [projects, setProjects] = useState([])
+
+  const getData = async() => {
+    try {
+      const response = await client.getEntries({ content_type: 'projects' });
+      const projects = response.items.map((item) => {
+        const {title, url, image} = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return {title, url, id, img};
+      })
+      setProjects(projects);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+};
